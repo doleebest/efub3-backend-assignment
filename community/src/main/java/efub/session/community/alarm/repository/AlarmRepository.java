2b95c116a53e4dd100941bd757b2ae5d1379c263
So Jeong Lee <sj27419@gmail.com>
2023-05-29 19:56:41 +0900

    fix alarm jpa

--- community/src/main/java/efub/session/community/alarm/repository/AlarmRepository.java
+++ community/src/main/java/efub/session/community/alarm/repository/AlarmRepository.java
@@ -2,9 +2,10 @@ package efub.session.community.alarm.repository;
 
 import efub.session.community.alarm.domain.Alarm;
 import efub.session.community.account.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface AlarmRepository {
+public interface AlarmRepository extends JpaRepository<Alarm,Long> {
     List<Alarm> findAllByMember(Member member);
 }
