73e1e1bb335fb9d8ac9f989a239a831ff4bb12cc
Sojeong Lee <sj27419@gmail.com>
2023-10-31 19:58:57 +0900

    add 의존성주입

--- community/src/test/java/efub/session/community/account/controller/MemberControllerTest.java
+++ community/src/test/java/efub/session/community/account/controller/MemberControllerTest.java
@@ -1,11 +1,19 @@
 package efub.session.community.account.controller;
 
+import efub.session.community.account.service.MemberService;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class) // JUniit5와 Mockito를 연동하기
 class MemberControllerTest{
+    @InjectMocks
+    private MemberController userController;
+
+    @Mock
+    private MemberService userService;
 
 }
