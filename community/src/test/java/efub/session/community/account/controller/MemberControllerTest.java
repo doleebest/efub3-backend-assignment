b3c0da10b1330b6f831cf4ac6e75df065d780325
Sojeong Lee <sj27419@gmail.com>
2023-10-31 19:59:52 +0900

    add mockMVC; http호출

--- community/src/test/java/efub/session/community/account/controller/MemberControllerTest.java
+++ community/src/test/java/efub/session/community/account/controller/MemberControllerTest.java
@@ -1,10 +1,13 @@
 package efub.session.community.account.controller;
 
 import efub.session.community.account.service.MemberService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,5 +18,12 @@ class MemberControllerTest{
 
     @Mock
     private MemberService userService;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void init() {
+        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+    }
 
 }
