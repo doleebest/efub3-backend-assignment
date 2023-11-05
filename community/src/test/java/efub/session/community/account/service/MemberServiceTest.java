6c631b0109293a287b6f9f9e0056b2d80419ba24
Sojeong Lee <sj27419@gmail.com>
2023-11-06 01:44:45 +0900

    add mockMVC; MemberServiceTest.java

--- community/src/test/java/efub/session/community/account/service/MemberServiceTest.java
+++ community/src/test/java/efub/session/community/account/service/MemberServiceTest.java
@@ -1,15 +1,14 @@
 package efub.session.community.account.service;
 
 import efub.session.community.account.repository.MemberRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 class MemberServiceTest {
     @InjectMocks
@@ -22,15 +21,15 @@ class MemberServiceTest {
     void signUpSuccess() {
         when(memberRepository.existsByEmail(anyString())).thenReturn(false);
 
-        String response = memberService.signup("sj@ewhain.net", "sj0000", "ewha", "soso", "212121");
-        assertEquals("회원 가입이 완료되었습니다.", response);
+        String response = memberService.signup("sj@ewhain.com", "password1234", "ewha", "nick", "212121");
+        assertEquals("회원 가입 완료", response);
     }
 
     @Test
     void signUpFailure() {
         when(memberRepository.existsByEmail(anyString())).thenReturn(true);
 
-        memberService.signup("sj@ewhain.net", "sj0000", "ewha", "soso", "212121");
+        memberService.signup("sj@ewhain.com", "password1234", "ewha", "nick", "212121");
     }
 
 }
